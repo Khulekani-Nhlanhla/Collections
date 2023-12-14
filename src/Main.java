23ae58b8ecef83d4be51e9c663a2500c63a5c6ca
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-12-14 09:34:52 +0100

    This is for the Task3 I just replaced HashSet with TreeSet which makes the list alphabetically ordered and does not take duplicate.

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,39 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) {
 
-
     String s;
-    HashSet<String> HashList = new HashSet<>();
+    TreeSet<String> TreeList = new TreeSet<>();
+
     //This is the IO Character Based Stream way of reading a File.
     try(BufferedReader Br = new BufferedReader(new FileReader("Employees.txt")))
     {
         while ((s = Br.readLine()) != null){
-            HashList.add(s);
+            TreeList.add(s);
 
         }
 
     }catch(IOException e){
         System.out.println("Data Loading Error");
     }
-        System.out.printf("The Size of the List is: " + HashList.size()+"\n");
+        System.out.printf("The Size of the List is: " + TreeList.size()+"\n");
         System.out.println("");
-        for (String element:HashList) {
+        for (String element:TreeList) {
             System.out.println(element);
         }
+        System.out.println(".");
         System.out.println("This is using the iterator");
         System.out.println(".");
-        Iterator<String> iter = HashList.iterator();
+        Iterator<String> iter = TreeList.iterator();
         while(iter.hasNext()){
             String element = iter.next();
             System.out.println(element);
         }
+
 
 
 
