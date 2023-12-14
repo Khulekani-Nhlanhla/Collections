83307da7c11f48cbfe5f525d3536b60ff0b03b9a
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-12-14 09:13:38 +0100

    This is the Commit for Task2 it is in order to replace the ArrayList with a HashSet.

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 
 public class Main {
@@ -10,25 +11,26 @@ public class Main {
 
 
     String s;
-    ArrayList<String> list = new ArrayList<>();
+    HashSet<String> HashList = new HashSet<>();
     //This is the IO Character Based Stream way of reading a File.
     try(BufferedReader Br = new BufferedReader(new FileReader("Employees.txt")))
     {
         while ((s = Br.readLine()) != null){
-            list.add(s);
+            HashList.add(s);
 
         }
 
     }catch(IOException e){
         System.out.println("Data Loading Error");
     }
-        System.out.printf("The Size of the List is: " + list.size()+"\n");
+        System.out.printf("The Size of the List is: " + HashList.size()+"\n");
         System.out.println("");
-        for (String element:list) {
+        for (String element:HashList) {
             System.out.println(element);
         }
         System.out.println("This is using the iterator");
-        Iterator<String> iter = list.iterator();
+        System.out.println(".");
+        Iterator<String> iter = HashList.iterator();
         while(iter.hasNext()){
             String element = iter.next();
             System.out.println(element);
