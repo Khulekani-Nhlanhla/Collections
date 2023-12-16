398cbdb60a04457e3b95b5873993924e8c6f6f0a
Kxxle <64878567+Kxxle@users.noreply.github.com>
2023-12-16 18:47:01 +0100

    This is for the Task4 I added a way to sort the array list using the sort method and I also reversed it to go from A-Z , to Z-A.

--- src/Main.java
+++ src/Main.java
@@ -2,39 +2,44 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.TreeSet;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
     String s;
-    TreeSet<String> TreeList = new TreeSet<>();
+    ArrayList<String> list = new ArrayList<>();
 
     //This is the IO Character Based Stream way of reading a File.
     try(BufferedReader Br = new BufferedReader(new FileReader("Employees.txt")))
     {
         while ((s = Br.readLine()) != null){
-            TreeList.add(s);
+            list.add(s);
 
         }
 
     }catch(IOException e){
         System.out.println("Data Loading Error");
     }
-        System.out.printf("The Size of the List is: " + TreeList.size()+"\n");
-        System.out.println("");
-        for (String element:TreeList) {
+        System.out.printf("The Size of the List is: " + list.size()+"\n");
+        System.out.println("This is the original list\n"+".");
+        for (String element:list) {
             System.out.println(element);
         }
         System.out.println(".");
-        System.out.println("This is using the iterator");
-        System.out.println(".");
-        Iterator<String> iter = TreeList.iterator();
+        Collections.sort(list);
+        System.out.println("This is the result after sorting the array list: \n"+".");
+
+        Iterator<String> iter = list.iterator();
         while(iter.hasNext()){
             String element = iter.next();
+            System.out.println(element);
+        }
+        System.out.println(".");
+        System.out.println("This is the for loop:\n"+".");
+        Collections.sort(list, Collections.reverseOrder());
+        for (int i = 0; i < list.size();i++){
+            String element = list.get(i);
             System.out.println(element);
         }
 
